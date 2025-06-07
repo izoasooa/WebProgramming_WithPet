deb9bfa5543bcee6defb696d2a4a3a9dd14675e2
hypark53 <rdpark@bible.ac.kr>
2025-06-07 17:20:04 +0900

    Update dog_behavior.js

--- 행동패턴/dog_behavior.js
+++ 행동패턴/dog_behavior.js
@@ -1,32 +1,11 @@
 const videos = [
-  {
-    src: "https://www.youtube.com/embed/MUAvchijfEU",
-    desc: "강아지가 밤에 우는 이유"
-  },
-  {
-    src: "https://www.youtube.com/embed/1TSykK48zD8",
-    desc: "강아지의 꼬리 흔들기"
-  },
-  {
-    src: "https://www.youtube.com/embed/v1nkM43QRAk",
-    desc: "강아지가 만지면 싫어하는 부위"
-  },
-  {
-    src: "https://www.youtube.com/embed/rqKQphyQn-c",
-    desc: "강아지의 사춘기"
-  },
-  {
-    src: "https://www.youtube.com/embed/ZKqPZooRchk",
-    desc: "치료가 필요한 강아지의 문제 행동"
-  },
-  {
-    src: "https://www.youtube.com/embed/4XD_xAfHd3M",
-    desc: "강아지가 싫어하는 사람의 행동"
-  },
-  {
-    src: "https://www.youtube.com/embed/9DfjPlLLRGA",
-    desc: "강아지의 마음"
-  }
+  { src: "https://www.youtube.com/embed/MUAvchijfEU", desc: "강아지가 밤에 우는 이유" },
+  { src: "https://www.youtube.com/embed/1TSykK48zD8", desc: "강아지의 꼬리 흔들기" },
+  { src: "https://www.youtube.com/embed/v1nkM43QRAk", desc: "강아지가 만지면 싫어하는 부위" },
+  { src: "https://www.youtube.com/embed/rqKQphyQn-c", desc: "강아지의 사춘기" },
+  { src: "https://www.youtube.com/embed/ZKqPZooRchk", desc: "치료가 필요한 강아지의 문제 행동" },
+  { src: "https://www.youtube.com/embed/4XD_xAfHd3M", desc: "강아지가 싫어하는 사람의 행동" },
+  { src: "https://www.youtube.com/embed/9DfjPlLLRGA", desc: "강아지의 마음" }
 ];
 
 let currentPage = 0;
